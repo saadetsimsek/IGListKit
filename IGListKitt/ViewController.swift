2eb31dc2fecac22dfd03465c1a8199bd1e18116e
saadetsimsek <saadetsimsek13@gmail.com>
2024-08-18 23:49:20 +0300

    Initial Commit

---
+++ IGListKitt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IGListKitt
+//
+//  Created by Saadet Şimşek on 18/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
